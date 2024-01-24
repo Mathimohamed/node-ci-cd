fd96dd1a3dacc6b1153d49b88f1093a2de702c1d
Mathimohamed <mathimohamed@gmail.com>
2024-01-24 13:11:24 +0530

    update

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello Welcome to tiruchirapalli !!');
-}).listen(8081);
+}).listen(9000);
--- dockerfile
+++ dockerfile
@@ -14,7 +14,7 @@ RUN npm install
 COPY . .
 
 # Expose the port the app runs on
-EXPOSE 8081
+EXPOSE 9000
 
 # Define the command to run your app
 CMD ["node", "app.js"]
